13ef89f4822591e3ac8b5e11aa2393f9ba5537ac
David Agudelo <tuemail@example.com>
2025-11-06 08:27:17 -0500

    update new version

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import en from "../../public/locales/en/common.json";
-import es from "../../public/locales/es/common.json";
+import en from "@/app/locales/en/common.json";
+import es from "@/app/locales/es/common.json";
 
 i18n
   .use(initReactI18next)
